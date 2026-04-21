b35a1fde9470be52c5966657486ef77eeeda45d2
SarithDedunu <kavindudedunupitiya@gmail.com>
2026-04-21 12:19:00 +0530

    Fix in ticket search, show and reply

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('tickets.create');
+    return view('home');
 });
 
 Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
 Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+
+Route::get('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
+Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
+
 Route::post('/tickets/{ticket}/replies', [TicketController::class, 'reply'])->name('tickets.reply');
