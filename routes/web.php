2aacdbedac50ee293b0d8071685adad82fe6004d
Kavindu <kavindudedunupitiya@gmail.com>
2026-04-21 23:09:30 +0530

    Fix routes conflict

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+
 Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
 
 Route::get('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
-<<<<<<< HEAD
+
 Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
 
 Route::post('/tickets/{ticket}/replies', [TicketController::class, 'reply'])->name('tickets.reply');
-=======
-Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
->>>>>>> 9ec915e15be4861c5c5869446f4dec50eb1a16f8
+
+
