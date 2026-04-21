610641c263e33acc0eacdd3d23430d31018f073b
SarithDedunu <kavindudedunupitiya@gmail.com>
2026-04-21 09:19:52 +0530

    Added home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Support System</title>
+</head>
+<body>
+
+<h1>Support System</h1>
+
+<a href="{{ route('tickets.create') }}">Open New Ticket</a>
+
+<hr>
+
+<h3>Check Your Ticket</h3>
+
+<form action="{{ route('tickets.search') }}" method="GET">
+    <input type="text" name="reference" placeholder="Enter ticket reference">
+    <button type="submit">Search</button>
+</form>
+
+</body>
+</html>
