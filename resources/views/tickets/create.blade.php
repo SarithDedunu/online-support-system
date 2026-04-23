b991f0e0dc4282d51542355093e5b0c75c7aec99
Kavindu <kavindudedunupitiya@gmail.com>
2026-04-23 14:17:15 +0530

    Add required field indicators and validation error messages

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -12,14 +12,51 @@
     </div>
 @endif
 
+@if($errors->any())
+    <div class="alert alert-danger">
+        <strong>Please fix the following errors:</strong>
+        <ul class="mb-0">
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form method="POST" action="{{ route('tickets.store') }}">
     @csrf
 
-    <input class="form-control mb-2" name="customer_name" placeholder="Name">
-    <input class="form-control mb-2" name="email" placeholder="Email">
-    <input class="form-control mb-2" name="phone" placeholder="Phone">
-    <input class="form-control mb-2" name="subject" placeholder="Subject">
-    <textarea class="form-control mb-2" name="description" placeholder="Description"></textarea>
+    <label>Name <span class="text-danger">*</span></label>
+    <input class="form-control mb-2 @error('customer_name') is-invalid @enderror"
+           name="customer_name" value="{{ old('customer_name') }}">
+    @error('customer_name')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+
+    <label>Email <span class="text-danger">*</span></label>
+    <input class="form-control mb-2 @error('email') is-invalid @enderror"
+           name="email" value="{{ old('email') }}">
+    @error('email')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+
+    <label>Phone</label>
+    <input class="form-control mb-2"
+           name="phone" value="{{ old('phone') }}">
+
+    <label>Subject <span class="text-danger">*</span></label>
+    <input class="form-control mb-2 @error('subject') is-invalid @enderror"
+           name="subject" value="{{ old('subject') }}">
+    @error('subject')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+
+    <label>Description <span class="text-danger">*</span></label>
+    <textarea class="form-control mb-2 @error('description') is-invalid @enderror"
+              name="description">{{ old('description') }}</textarea>
+    @error('description')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
 
     <button class="btn btn-primary">Submit</button>
 </form>
